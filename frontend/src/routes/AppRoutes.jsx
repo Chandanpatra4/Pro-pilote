f869aab036fc04fdf4380bebc232a51db68e8205
Chandanpatra4 <165011332+Chandanpatra4@users.noreply.github.com>
2025-04-27 06:26:29 +0530

    created login,register api and some frontend

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
+import Home from "../screens/Home";
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
